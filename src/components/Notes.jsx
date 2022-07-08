97c3640f190b2340af898b9203bf1d1b646606ec
ENSANITYel <96373614+ENSANITYel@users.noreply.github.com>
2022-07-08 20:54:42 +0530

    Update Notes.jsx

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -5,7 +5,7 @@ function Notes() {
     <div className="note">
       <h1>Sabyasachi Dutta</h1>
       <p>
-        This is notes making front end project based on react from devtown on
+        This is notes making front end project based on react  on
         note making webapp
       </p>
     </div>
